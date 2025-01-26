fc327c675655d361642b692aeb4bdba2252f5ec5
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-01-26 22:27:57 +0800

    [desktop] updated file structure

---
+++ raspberrypi/app/config.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+class Config:
+	def __init__(self, root: tk.Tk):
+		self.root = root
+
+	def set_fullscreen(self):
+		self.root.attributes('-fullscreen', True)
+		self.root.bind('<F11>', self.toggle_fullscreen)
+
+	def toggle_fullscreen(self, event=None):
+		current_state = self.root.attributes('-fullscreen')
+		self.root.attributes('-fullscreen', not current_state)
+
+BASE_URL = 'http://localhost:8000/api/'
