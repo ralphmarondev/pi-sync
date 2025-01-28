09ba3fb5d6f76e697eb82bd0c128aa54d99192e9
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-01-28 21:57:29 +0800

    [android] updated SetupScreen
    - We implemented the field, description and button to save new
      ipaddress.
    - Added SetupViewModel to handle the logic.

--- android/app/src/main/java/com/ralphmarondev/pisync/features/setup/presentation/SetupScreen.kt
+++ android/app/src/main/java/com/ralphmarondev/pisync/features/setup/presentation/SetupScreen.kt
@@ -1,27 +1,43 @@
 package com.ralphmarondev.pisync.features.setup.presentation
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SetupScreen(
     navigateBack: () -> Unit
 ) {
+    val viewModel: SetupViewModel = viewModel()
+    val ipAddress by viewModel.ipAddress.collectAsState()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -51,11 +67,50 @@ fun SetupScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+                .padding(16.dp)
         ) {
             Text(
-                text = "Setup server ip address here.",
-                fontFamily = FontFamily.Monospace
+                text = "Please enter the IP address of the server that you want to connect to.",
+                fontFamily = FontFamily.Monospace,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.W400,
+                color = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.padding(start = 4.dp)
             )
+
+            OutlinedTextField(
+                value = ipAddress,
+                onValueChange = { viewModel.onIpAddressChange(it) },
+                label = { Text("Enter IP Address") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                isError = ipAddress.isEmpty(),
+                supportingText = {
+                    if (ipAddress.isEmpty()) {
+                        Text("IP Address cannot be empty", color = MaterialTheme.colorScheme.error)
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                ),
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                onClick = viewModel::saveIpAddress,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = "Save IP Address",
+                    fontFamily = FontFamily.Monospace,
+                    fontWeight = FontWeight.W500,
+                    fontSize = 16.sp
+                )
+            }
         }
     }
 }
