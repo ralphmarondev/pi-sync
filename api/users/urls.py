0ffa1b8964ea90aaa2da41fcea0343314df7882d
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-01-28 10:27:58 +0800

    [api] updated users
    - Implemented updating, deleting and viewing list of user/s on the api.

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import *
 
+from .views import *
 urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='user_login'),
-    path('register/', UserRegisterView.as_view(), name='user_register')
+	path('login/', UserLoginView.as_view(), name='user_login'),
+	path('register/', UserRegisterView.as_view(), name='user_register'),
+	path('users/', UserListView.as_view(), name='users_list'),
+	path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+	path('user/update/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
+	path('user/delete/<int:pk>/', UserDeleteView.as_view(), name='user_delete')
 ]
--- api/users/views.py
+++ api/users/views.py
@@ -51,3 +51,102 @@ class UserLoginView(APIView):
 			'success': False,
 			'message': 'Invalid password'
 		}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserListView(APIView):
+	def get(self, request):
+		users = User.objects.filter(is_deleted=False)
+		serializer = UserSerializer(users, many=True)
+
+		return Response(
+			data={
+				'success': True,
+				'message': 'Users retrieved successfully',
+				'users': serializer.data
+			},
+			status=status.HTTP_200_OK
+		)
+
+class UserDetailView(APIView):
+	def get(self, request, pk):
+		try:
+			user = User.objects.get(pk=pk, is_deleted=False)
+		except User.DoesNotExist:
+			return Response(
+				data={
+					'success': False,
+					'message': 'User not found'
+				},
+				status=status.HTTP_404_NOT_FOUND
+			)
+
+		serializer = UserSerializer(user)
+		return Response(
+			data={
+				'success': True,
+				'message': serializer.data
+			},
+			status=status.HTTP_200_OK
+		)
+
+class UserUpdateView(APIView):
+	def put(self, request, pk):
+		try:
+			user = User.objects.get(pk=pk)
+		except  User.DoesNotExists:
+			return Response(
+				data={
+					'success': False,
+					'message': 'User not found'
+				},
+				status=status.HTTP_404_NOT_FOUND
+			)
+
+		data = request.data
+		serializer = UserSerializer(user, data=data, partial=True)
+
+		if serializer.is_valid():
+			user = serializer.save()
+			user.set_password(data.get('password', user.password))
+			user.save()
+
+			registered_doors = data.get('registered_doors', [])
+			if registered_doors:
+				user.registered_doors.set(registered_doors)
+			return Response(
+				data={
+					'success': True,
+					'message': 'User updated successfully'
+				},
+				status=status.HTTP_200_OK
+			)
+		return Response(
+			data={
+				'success': False,
+				'message': 'Update failed',
+				'errors': serializer.errors
+			}
+		)
+
+class UserDeleteView(APIView):
+	def delete(self, request, pk):
+		try:
+			user = User.objects.get(pk=pk)
+		except User.DoesNotExist:
+			return Response(
+				data={
+					'success': False,
+					'message': 'User not found'
+				},
+				status=status.HTTP_404_NOT_FOUND
+			)
+
+		user.is_deleted = True
+		user.save()
+
+		return Response(
+			data={
+				'success': True,
+				'message': 'User deleted successfully'
+			},
+			status=status.HTTP_200_OK
+		)
