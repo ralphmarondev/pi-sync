d7c1086fca66415cc674d878aa52a57ef2873122
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-02-08 12:20:57 +0800

    [desktop] updated about_screen.py

--- raspberrypi/app/about/about_screen.py
+++ raspberrypi/app/about/about_screen.py
@@ -8,8 +8,36 @@ class AboutScreen:
 		dashboard_frame = tk.Frame(self.root)
 		lb = tk.Label(
 			master=dashboard_frame,
-			text='About Screen',
+			text='Members:',
 			font=('Bold', 30)
 		)
+		lb.pack(pady=5)
+
+		members = [
+			'Ralph Maron Eda',
+			'Jack Cabigayan',
+			'Triesha Mae Olunan',
+			'Jezlyn Cabbab'
+		]
+		for i in range(len(members)):
+			lb = tk.Label(
+				master = dashboard_frame,
+				text=members[i],
+				font=('monospace', 20)
+			)
+			lb.pack()
+
+		adviser_lbl = tk.Label(
+			master=dashboard_frame,
+			text='Adviser:',
+			font=('Bold', 30)
+		)
+		adviser_lbl.pack(pady=5)
+		lb = tk.Label(
+			master=dashboard_frame,
+			text='Engr. Jerome Paul Viador',
+			font=('monospace', 20)
+		)
 		lb.pack()
+
 		return dashboard_frame
