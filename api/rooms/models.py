a2d493cd60527a56bf04932e279288426ae26da1
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-02-15 15:51:04 +0800

    [api] updated Door model
    - Added `is_open` field.
    - It supports closing and opening door now.

--- api/rooms/models.py
+++ api/rooms/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class Door(models.Model):
-	id = models.AutoField(primary_key=True)
-	name = models.CharField(max_length=200, null=True, blank=True)
-	is_active = models.BooleanField(default=True)
-	create_date = models.DateTimeField(auto_now=True)
-	created_by = models.CharField(max_length=200, blank=True, null=True)
-	is_deleted = models.BooleanField(default=False)
-	update_date = models.DateTimeField(auto_now=True)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_open = models.BooleanField(default=False)
+    create_date = models.DateTimeField(auto_now=True)
+    created_by = models.CharField(max_length=200, blank=True, null=True)
+    is_deleted = models.BooleanField(default=False)
+    update_date = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        state = 'Open' if self.is_open else 'Closed'
+        return f'{self.name} - {state}'
--- api/rooms/urls.py
+++ api/rooms/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import DoorView
+from .views import DoorView, OpenDoorView, CloseDoorView
+
 urlpatterns = [
-	path('doors/', DoorView.as_view(), name='door_list'),
-	path('door/<int:pk>/', DoorView.as_view(), name='door_detail')
+    path('doors/', DoorView.as_view(), name='door_list'),
+    path('door/<int:pk>/', DoorView.as_view(), name='door_detail'),
+    path('door/<int:pk>/open/', OpenDoorView.as_view(), name='door_open'),
+    path('door/<int:pk>/close/', CloseDoorView.as_view(), name='door_close'),
 ]
