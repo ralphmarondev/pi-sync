2bbc6ed50cc4ce0fa9290e96a21921362b27d6d6
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-02-16 13:07:30 +0800

    [android] updated SetupIpDialog

--- android/app/src/main/java/com/ralphmarondev/pisync/features/auth/presentation/components/SetupIpDialog.kt
+++ android/app/src/main/java/com/ralphmarondev/pisync/features/auth/presentation/components/SetupIpDialog.kt
@@ -1,16 +1,20 @@
 package com.ralphmarondev.pisync.features.auth.presentation.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Clear
-import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -31,13 +36,17 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import coil.compose.rememberAsyncImagePainter
+import com.ralphmarondev.pisync.MyApp
+import com.ralphmarondev.pisync.R
 
 @Composable
 fun SetupIpDialog(
     onDismiss: () -> Unit,
     onSave: (String) -> Unit
 ) {
-    var serverIp by remember { mutableStateOf("") }
+    val preferences = MyApp.preferences
+    var serverIp by remember { mutableStateOf(preferences.getIpAddress() ?: "") }
 
     Dialog(
         onDismissRequest = onDismiss,
@@ -51,84 +60,101 @@ fun SetupIpDialog(
                 .fillMaxWidth()
                 .padding(8.dp)
         ) {
-            Row(
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = "Settings",
-                    fontFamily = FontFamily.Monospace,
-                    fontWeight = FontWeight.W500,
-                    fontSize = 18.sp,
-                    color = MaterialTheme.colorScheme.primary,
+                Image(
+                    painter = rememberAsyncImagePainter(R.drawable.ip_address),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .padding(start = 8.dp)
+                        .size(60.dp)
                 )
 
-                IconButton(onClick = onDismiss) {
-                    Icon(
-                        imageVector = Icons.Outlined.Close,
-                        contentDescription = "Close"
-                    )
-                }
-            }
-            HorizontalDivider(modifier = Modifier.padding(vertical = 2.dp))
-            OutlinedTextField(
-                value = serverIp,
-                onValueChange = { serverIp = it },
-                label = {
-                    Text(
-                        text = "Enter your server ip",
-                        fontFamily = FontFamily.Monospace,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                },
-                textStyle = TextStyle(
-                    fontFamily = FontFamily.Monospace,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.W500
-                ),
-                placeholder = {
-                    Text(
-                        text = "192.68.xx.xx",
-                        fontFamily = FontFamily.Monospace,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                },
-                maxLines = 1,
-                trailingIcon = {
-                    AnimatedVisibility(serverIp.isNotEmpty()) {
-                        IconButton(onClick = { serverIp = "" }) {
-                            Icon(
-                                imageVector = Icons.Outlined.Clear,
-                                contentDescription = "Clear"
-                            )
-                        }
-                    }
-                },
-                modifier = Modifier
-                    .padding(horizontal = 16.dp, vertical = 4.dp)
-            )
-
-            Button(
-                onClick = {
-                    if (serverIp.isNotEmpty()) {
-                        onSave(serverIp)
-                    }
-                },
-                modifier = Modifier.padding(16.dp)
-            ) {
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(
-                    text = "SAVE",
+                    text = "Setup Server",
                     fontFamily = FontFamily.Monospace,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.W500
+                    fontSize = 14.sp,
+                    color = MaterialTheme.colorScheme.secondary
                 )
+
+                Spacer(modifier = Modifier.height(16.dp))
+                OutlinedTextField(
+                    value = serverIp,
+                    onValueChange = { serverIp = it },
+                    textStyle = TextStyle(
+                        fontFamily = FontFamily.Monospace,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.W500
+                    ),
+                    placeholder = {
+                        Text(
+                            text = "192.68.xx.xx",
+                            fontFamily = FontFamily.Monospace,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = "IP Address",
+                            fontFamily = FontFamily.Monospace,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    },
+                    singleLine = true,
+                    trailingIcon = {
+                        AnimatedVisibility(serverIp.isNotEmpty()) {
+                            IconButton(onClick = { serverIp = "" }) {
+                                Icon(
+                                    imageVector = Icons.Outlined.Clear,
+                                    contentDescription = "Clear"
+                                )
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 4.dp)
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Button(
+                        onClick = {
+                            if (serverIp.isNotEmpty()) {
+                                onSave(serverIp)
+                            }
+                        },
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text(
+                            text = " SAVE ",
+                            fontFamily = FontFamily.Monospace,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.W500
+                        )
+                    }
+
+                    ElevatedButton(
+                        onClick = onDismiss
+                    ) {
+                        Text(
+                            text = "CANCEL",
+                            fontFamily = FontFamily.Monospace,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.W500
+                        )
+                    }
+                }
             }
         }
     }
