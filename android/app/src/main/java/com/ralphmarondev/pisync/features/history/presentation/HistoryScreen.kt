e4159153ba723e6e138215ac078b273ec977da68
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-02-16 17:37:26 +0800

    [android] added refresh button on HistoryScreen

--- android/app/src/main/java/com/ralphmarondev/pisync/features/history/presentation/HistoryScreen.kt
+++ android/app/src/main/java/com/ralphmarondev/pisync/features/history/presentation/HistoryScreen.kt
@@ -4,7 +4,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,9 +34,22 @@ fun HistoryScreen() {
                         text = "History"
                     )
                 },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            viewModel.fetchDoorLogs()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Refresh,
+                            contentDescription = "Refresh history"
+                        )
+                    }
+                },
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primary,
-                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary
                 )
             )
         }
