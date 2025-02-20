58cd5c39aaf1e9d4000112d9e2a53aead2b26820
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-02-20 17:11:31 +0800

    [android] updated AboutScreen

--- android/app/src/main/java/com/ralphmarondev/pisync/features/settings/presentation/about/AboutScreen.kt
+++ android/app/src/main/java/com/ralphmarondev/pisync/features/settings/presentation/about/AboutScreen.kt
@@ -1,11 +1,22 @@
 package com.ralphmarondev.pisync.features.settings.presentation.about
 
-import androidx.compose.foundation.layout.Box
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -16,16 +27,24 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.ralphmarondev.pisync.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen(
     navigateBack: () -> Unit
 ) {
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -52,20 +71,92 @@ fun AboutScreen(
             )
         }
     ) { innerPadding ->
-        Box(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
-            contentAlignment = Alignment.Center
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp),
         ) {
-            Text(
-                text = "About Screen",
-                fontWeight = FontWeight.W500,
-                fontSize = 20.sp,
-                color = MaterialTheme.colorScheme.secondary,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(16.dp)
-            )
+            item { Spacer(modifier = Modifier.height(16.dp)) }
+            item {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(R.drawable.app_icon),
+                        contentDescription = "App icon",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(120.dp)
+                            .clip(CircleShape)
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = stringResource(R.string.app_name),
+                        fontWeight = FontWeight.W500,
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+
+                    Text(
+                        text = "Version 1.0",
+                        fontWeight = FontWeight.W500,
+                        fontSize = 14.sp,
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colorScheme.tertiary
+                    )
+                }
+                HorizontalDivider(modifier = Modifier.padding(16.dp))
+
+                Text(
+                    text = "Overview:",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.W500,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Text(
+                    text = stringResource(R.string.project_overview),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W400,
+                    color = MaterialTheme.colorScheme.secondary,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+                HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+
+                Text(
+                    text = "Source Code:",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.W500,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Text(
+                    text = "Available on GitHub!",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W400,
+                    color = MaterialTheme.colorScheme.secondary,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+
+                Text(
+                    text = "You can download it here!",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W300,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    modifier = Modifier
+                        .clickable {
+                            val intent = Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("https://github.com/ralphmarondev/pi-sync")
+                            )
+                            context.startActivity(intent)
+                        }
+                )
+            }
+            item { Spacer(modifier = Modifier.height(100.dp)) }
         }
     }
 }
