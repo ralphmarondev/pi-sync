58fefb7c4e1475d9f3d0e3d42715356cd64471fd
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-02-20 17:16:10 +0800

    [android] updated theme to make statusbar color more cute

--- android/app/src/main/java/com/ralphmarondev/pisync/ui/theme/Theme.kt
+++ android/app/src/main/java/com/ralphmarondev/pisync/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.ralphmarondev.pisync.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -47,6 +51,17 @@ fun PiSyncTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            val insetsController = window?.let {
+                WindowCompat.getInsetsController(it, view)
+            }
+            insetsController?.isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
