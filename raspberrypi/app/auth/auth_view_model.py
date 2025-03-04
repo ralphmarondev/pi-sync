34e38c3e0ad7b3afc328479fce4cc1fdf41c111a
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-03-04 17:49:29 +0800

    [desktop] removed api connection for ui testing

--- raspberrypi/app/auth/auth_view_model.py
+++ raspberrypi/app/auth/auth_view_model.py
@@ -1,5 +1,3 @@
-from constants import API_BASE_URL
-
 class AuthViewModel:
     def __init__(self):
         self.username = ''
@@ -15,22 +13,27 @@ class AuthViewModel:
         if not self.username.strip() or not self.password.strip():
             return False, 'Username or password cannot be empty!'
 
-        try:
-            response = requests.post(
-                url=f'{API_BASE_URL}login/',
-                data={'username': self.username, 'password': self.password}
-            )
-            if response.status_code == 200:
-                print(f'Login successful:', response.json())
-                return True, None
-            else:
-                try:
-                    error_message = response.json().get('message', 'Login failed')
-                except ValueError:
-                    error_message = 'Login failed with unexpected response'
-                print('Login failed:', response.status_code, error_message)
-                return False, error_message
-        except requests.RequestException as e:
-            message = f'Error connecting to the server: {e}'
-            print(message)
-            return False, message
+        # for testing
+        if self.username.strip() == 'ralphmaron' and self.password.strip() == 'iscute':
+            return True, 'Login successful!'
+        return False, 'Login failed! Incorrect credentials.'
+
+        # try:
+        #     response = requests.post(
+        #         url=f'{API_BASE_URL}login/',
+        #         data={'username': self.username, 'password': self.password}
+        #     )
+        #     if response.status_code == 200:
+        #         print(f'Login successful:', response.json())
+        #         return True, None
+        #     else:
+        #         try:
+        #             error_message = response.json().get('message', 'Login failed')
+        #         except ValueError:
+        #             error_message = 'Login failed with unexpected response'
+        #         print('Login failed:', response.status_code, error_message)
+        #         return False, error_message
+        # except requests.RequestException as e:
+        #     message = f'Error connecting to the server: {e}'
+        #     print(message)
+        #     return False, message
