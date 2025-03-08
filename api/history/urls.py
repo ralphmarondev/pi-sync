7c7ea1a484185ef48c3bd856cbecf22c5f857432
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-03-08 12:13:36 +0800

    [api] update history to return only the history of a specific room

--- api/history/urls.py
+++ api/history/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HistoryView
+from .views import HistoryView, RoomHistoryView
 
 urlpatterns = [
     path('history/', HistoryView.as_view(), name='history_list'),
-    path('history/<int:pk>/', HistoryView.as_view(), name='history_detail')
+    path('history/<int:pk>/', HistoryView.as_view(), name='history_detail'),
+    path('history/room/<int:room_id>/', RoomHistoryView.as_view(), name='history_room')
 ]
