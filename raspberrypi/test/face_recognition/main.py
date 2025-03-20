166f941e4241e6deebb3ee81666285f2409dec76
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-03-20 18:34:29 +0800

    [desktop] testing with Marylitte Capistrano hahaha

--- raspberrypi/test/face_recognition/main.py
+++ raspberrypi/test/face_recognition/main.py
@@ -10,7 +10,7 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 counter = 0
 face_match = False
 
-reference_image = cv2.imread('reference2.jpg')
+reference_image = cv2.imread('reference3.jpg')
 
 def check_face(frame):
 	global face_match
