f9fb56ab5e1903a0b9fb64b2e18bda91d8a9d6f2
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-03-25 11:28:51 +0800

    [api] updated room view and urls

--- api/rooms/urls.py
+++ api/rooms/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import DoorView, OpenDoorView, CloseDoorView, DoorStatusView
+from .views import *
 
 urlpatterns = [
-    path('doors/', DoorView.as_view(), name='door_list'),
-    path('door/<int:pk>/', DoorView.as_view(), name='door_detail'),
+    path('doors/', DoorListView.as_view(), name='door_list'),
+    path('door/<int:pk>/', DoorDetailView.as_view(), name='door_detail'),
+    path('door/new/', NewDoorView.as_view(), name='new-door'),
+    path('door/update/<int:pk>/', UpdateDoorView.as_view(), name='update-door'),
+    path('door/delete/<int:pk>/', DeleteDoorView.as_view(), name='delete-door'),
     path('door/open/<int:pk>/', OpenDoorView.as_view(), name='door_open'),
     path('door/close/<int:pk>/', CloseDoorView.as_view(), name='door_close'),
     path('door/status/<int:pk>/', DoorStatusView.as_view(), name='door_status')
