dae29a7204d723c8678e02e41774e4caecee2ddc
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-04-08 21:00:51 +0800

    [android] getting the real username of the current active user

--- android/app/src/main/java/com/ralphmarondev/pisync/features/overview/presentation/OverviewViewModel.kt
+++ android/app/src/main/java/com/ralphmarondev/pisync/features/overview/presentation/OverviewViewModel.kt
@@ -3,6 +3,7 @@ package com.ralphmarondev.pisync.features.overview.presentation
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ralphmarondev.pisync.core.data.local.preferences.AppPreferences
 import com.ralphmarondev.pisync.features.overview.domain.model.Door
 import com.ralphmarondev.pisync.features.overview.domain.usecases.CloseDoorByIdUseCase
 import com.ralphmarondev.pisync.features.overview.domain.usecases.GetDoorsByUsernameUseCase
@@ -14,28 +15,40 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 class OverviewViewModel(
+    private val preferences: AppPreferences,
     private val getDoorsByUsernameUseCase: GetDoorsByUsernameUseCase,
     private val closeDoorByIdUseCase: CloseDoorByIdUseCase,
     private val openDoorByIdUseCase: OpenDoorByIdUseCase
 ) : ViewModel() {
+
     private val _doors = MutableStateFlow<List<Door>>(emptyList())
     val doors = _doors.asStateFlow()
 
     private val _isLoading = MutableStateFlow(true)
     val isLoading = _isLoading.asStateFlow()
 
-    private val username = "ralphmaron"
+    private val _username = MutableStateFlow<String?>(null)
 
     init {
-        fetchUpdatesContinuously()
+        viewModelScope.launch {
+            _isLoading.value = true
+            _username.value = preferences.getCurrentUser()
+
+            if (_username.value == null) {
+                _isLoading.value = false
+                return@launch
+            }
+            fetchUpdatesContinuously()
+        }
     }
 
     private fun fetchUpdatesContinuously() {
         viewModelScope.launch {
-            _isLoading.value = true
             while (isActive) {
                 try {
-                    val updatedDoors = getDoorsByUsernameUseCase(username)
+                    val updatedDoors = getDoorsByUsernameUseCase(
+                        username = _username.value ?: "No username provided."
+                    )
                     _doors.value = updatedDoors
                     Log.d("App", "Update door status: ${updatedDoors.map { it.status }}")
                 } catch (e: Exception) {
@@ -49,14 +62,12 @@ class OverviewViewModel(
 
     fun setDoorStatus(id: Int) {
         viewModelScope.launch {
-            val username = "ralphmaron"
-
             val door = _doors.value.find { it.id == id } ?: return@launch
 
             if (door.status) {
-                closeDoorByIdUseCase(id, username)
+                closeDoorByIdUseCase(id = id, username = _username.value ?: "No username provided.")
             } else {
-                openDoorByIdUseCase(id, username)
+                openDoorByIdUseCase(id = id, username = _username.value ?: "No username provided.")
             }
 
             _doors.value = _doors.value.map {
