edb8125723bf94b5b8628bf1dc9e863204131bad
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-04-09 15:30:15 +0800

    [raspberrypi] updated gpio pin to 17

--- raspberrypi/solenoid_test.py
+++ raspberrypi/solenoid_test.py
@@ -1,14 +1,16 @@
 import RPi.GPIO as GPIO 
 from time import sleep 
 
+DOOR_PIN = 17
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(DOOR_PIN, GPIO.OUT)
 
 while True:
     print('Opening solenoid...')
-    GPIO.output(18, 1)
+    GPIO.output(DOOR_PIN, 1)
     sleep(2)
     print('Closing solenoid...')
-    GPIO.output(18, 0)
+    GPIO.output(DOOR_PIN, 0)
     sleep(2)
