daa35bbd803e72b3ab4c0d7a5633acfc505b32bd
Jack Cabigayan <cabigayanjack01@gmail.com>
2025-04-22 14:27:09 +0800

    try

--- raspberrypi/solenoid_test.py
+++ raspberrypi/solenoid_test.py
@@ -2,12 +2,12 @@ import RPi.GPIO as GPIO
 import time
 
 # Constants
-RELAY_PIN = 17  
+RELAY_PIN = 17  # GPIO pin connected to relay IN
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
-GPIO.output(RELAY_PIN, GPIO.HIGH)  
+GPIO.output(RELAY_PIN, GPIO.LOW)  
 
 # Track current state
 is_unlocked = False
@@ -16,11 +16,11 @@ def toggle_solenoid():
     global is_unlocked
     if is_unlocked:
         # Lock it
-        GPIO.output(RELAY_PIN, GPIO.HIGH)
+        GPIO.output(RELAY_PIN, GPIO.LOW)
         print("🔒 Solenoid is now LOCKED.")
     else:
         # Unlock it
-        GPIO.output(RELAY_PIN, GPIO.LOW)
+        GPIO.output(RELAY_PIN, GPIO.HIGH)
         print("🔓 Solenoid is now UNLOCKED.")
     is_unlocked = not is_unlocked
 
