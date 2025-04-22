c1d0e9f06f59c2b4bd6029bfe99fcc9d8e868e50
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-04-22 23:06:55 +0800

    [desktop] added navigation bar on homescreen

--- desktop/Home/HomeForm.Designer.cs
+++ desktop/Home/HomeForm.Designer.cs
@@ -28,34 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            panel1 = new Panel();
+            button1 = new Button();
+            panel2 = new Panel();
+            label3 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(354, 205);
-            label1.Name = "label1";
-            label1.Size = new Size(166, 25);
-            label1.TabIndex = 0;
-            label1.Text = "Home Screen";
+            panel1.BackColor = Color.GhostWhite;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(241, 518);
+            panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Lavender;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Courier New", 14F);
+            button1.Location = new Point(12, 96);
+            button1.Margin = new Padding(0);
+            button1.Name = "button1";
+            button1.Size = new Size(212, 61);
+            button1.TabIndex = 1;
+            button1.Text = "DASHBOARD";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.MediumPurple;
+            panel2.Controls.Add(label3);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(241, 79);
+            panel2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Courier New", 16F);
+            label3.ForeColor = Color.Lavender;
+            label3.Location = new Point(59, 24);
+            label3.Name = "label3";
+            label3.Size = new Size(109, 30);
+            label3.TabIndex = 1;
+            label3.Text = "PISYNC";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Lavender;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Courier New", 14F);
+            button2.Location = new Point(12, 167);
+            button2.Margin = new Padding(0);
+            button2.Name = "button2";
+            button2.Size = new Size(212, 61);
+            button2.TabIndex = 2;
+            button2.Text = "ROOMS";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Lavender;
+            button3.Cursor = Cursors.Hand;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Courier New", 14F);
+            button3.Location = new Point(12, 239);
+            button3.Margin = new Padding(0);
+            button3.Name = "button3";
+            button3.Size = new Size(212, 61);
+            button3.TabIndex = 3;
+            button3.Text = "TENANTS";
+            button3.UseVisualStyleBackColor = true;
             // 
             // HomeForm
             // 
             AutoScaleDimensions = new SizeF(14F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(941, 518);
-            Controls.Add(label1);
+            Controls.Add(panel1);
             Font = new Font("Courier New", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
             Margin = new Padding(5, 4, 5, 4);
             Name = "HomeForm";
             Text = "HomeForm";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private Label label3;
+        private Button button3;
+        private Button button2;
     }
 }
