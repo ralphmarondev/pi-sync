ab3e59a87e10d407387e89e64fb541e410a12a81
Jack Cabigayan <cabigayanjack01@gmail.com>
2025-04-22 14:51:56 +0800

    [raspberrypi] some updates

--- raspberrypi/solenoid_test.py
+++ raspberrypi/solenoid_test.py
@@ -1,8 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set the GPIO pin number (same as Arduino pin 7 equivalent)
-RELAY_PIN = 7  # BCM numbering (not physical pin 7!)
+RELAY_PIN = 18  
 
 # Setup
 GPIO.setmode(GPIO.BCM)  # Use BCM numbering
@@ -11,16 +10,16 @@ GPIO.setup(RELAY_PIN, GPIO.OUT)
 try:
     while True:
         print("Relay ON")
-        GPIO.output(RELAY_PIN, GPIO.LOW)  # Active LOW: ON
+        GPIO.output(RELAY_PIN, GPIO.LOW)  
         time.sleep(2)
 
         print("Relay OFF")
-        GPIO.output(RELAY_PIN, GPIO.HIGH)  # OFF
+        GPIO.output(RELAY_PIN, GPIO.HIGH) 
         time.sleep(2)
 
 except KeyboardInterrupt:
     print("Exiting...")
 
 finally:
-    GPIO.output(RELAY_PIN, GPIO.HIGH)  # Make sure it's off
+    GPIO.output(RELAY_PIN, GPIO.HIGH)  
     GPIO.cleanup()
