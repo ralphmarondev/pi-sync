645262f0141e18136fdafcb1f8f7652691086f65
Jack Cabigayan <cabigayanjack01@gmail.com>
2025-04-22 15:45:24 +0800

    [raspberrypi] some updates

--- raspberrypi/solenoid_test.py
+++ raspberrypi/solenoid_test.py
@@ -7,10 +7,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay_pin, GPIO.OUT)
 
 while True:
-    GPIO.output(relay_pin, GPIO.HIGH)  # Should turn OFF the relay (green LED off)
+    GPIO.output(relay_pin, GPIO.LOW)  # Should turn OFF the relay (green LED off)
     print("Relay OFF")
-    time.sleep(2)
+    time.sleep(5)
     
-    GPIO.output(relay_pin, GPIO.LOW)   # Should turn ON the relay (green LED on)
+    GPIO.output(relay_pin, GPIO.HIGH)   # Should turn ON the relay (green LED on)
     print("Relay ON")
-    time.sleep(2)
+    time.sleep(5)
