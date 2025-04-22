8c48d03a6bb11ea88da332294ec2b3dd0216d0cf
Jack Cabigayan <cabigayanjack01@gmail.com>
2025-04-22 14:45:41 +0800

    [raspberrypi] some updates

--- raspberrypi/solenoid_test.py
+++ raspberrypi/solenoid_test.py
@@ -1,26 +1,16 @@
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO 
+from time import sleep 
 
-# Set the GPIO pin number (same as Arduino pin 7 equivalent)
-RELAY_PIN = 7  # BCM numbering (not physical pin 7!)
+DOOR_PIN = 18
 
-# Setup
-GPIO.setmode(GPIO.BCM)  # Use BCM numbering
-GPIO.setup(RELAY_PIN, GPIO.OUT)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DOOR_PIN, GPIO.OUT)
 
-try:
-    while True:
-        print("Relay ON")
-        GPIO.output(RELAY_PIN, GPIO.LOW)  # Active LOW: ON
-        time.sleep(2)
-
-        print("Relay OFF")
-        GPIO.output(RELAY_PIN, GPIO.HIGH)  # OFF
-        time.sleep(2)
-
-except KeyboardInterrupt:
-    print("Exiting...")
-
-finally:
-    GPIO.output(RELAY_PIN, GPIO.HIGH)  # Make sure it's off
-    GPIO.cleanup()
+while True:
+    print('Opening solenoid...')
+    GPIO.output(DOOR_PIN, 1)
+    sleep(2)
+    print('Closing solenoid...')
+    GPIO.output(DOOR_PIN, 0)
+    sleep(2)
