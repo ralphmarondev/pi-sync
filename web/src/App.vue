215a296aaf450844699012159b9f2abf1790d084
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-05-02 23:54:04 +0800

    removed web

--- web/src/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container mt-3">
-    <h3 class="text-primary">Hello there, Ralph Maron Eda is here!</h3>
-    <h5 class="text-secondary">Computer Engineering student.</h5>
-  </div>
-</template>
--- web/src/views/auth/AuthIndex.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        Auth
-    </div>
-</template>
