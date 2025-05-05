2857790b3fd54e69fae771595a6acff7f9281a1b
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-05-06 07:07:43 +0800

    [desktop] added NewTenantForm

---
+++ desktop/Tenant/NewTenantForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace PiSync.Tenant
+{
+    partial class NewTenantForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(224, 193);
+            label1.Name = "label1";
+            label1.Size = new Size(406, 22);
+            label1.TabIndex = 0;
+            label1.Text = "CREATING NEW TENAT GOES HERE HEHE";
+            // 
+            // NewTenantForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 22F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "NewTenantForm";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "NEW TENANT";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
