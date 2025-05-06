5b90ec81ce827690da1cfbf0a0fb876bdd29d540
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-05-06 11:53:31 +0800

    [desktop] stoped tracking .csproj.user and added to .gitignore

---
+++ desktop/Tenant/Delete/TenantDeleteForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PiSync.Tenant.Delete
+{
+    public partial class TenantDeleteForm : Form
+    {
+        public TenantDeleteForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
