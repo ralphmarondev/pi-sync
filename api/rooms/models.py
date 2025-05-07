68df86304b168733d82fa1f947f6d4f95c792e8a
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-05-07 11:02:36 +0800

    [api] made name of Room model unique

--- api/rooms/models.py
+++ api/rooms/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Door(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_open = models.BooleanField(default=False)
     create_date = models.DateTimeField(auto_now=True)
