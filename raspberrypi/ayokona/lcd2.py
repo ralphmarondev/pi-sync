4af6b6f86452be96e4cfa8dd17900b20956059c8
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-05-07 17:59:23 +0800

    [raspberrypi] added lcd2.py

---
+++ raspberrypi/ayokona/lcd2.py
@@ -0,0 +1,13 @@
+from RPLCD.i2c import CharLCD
+import time
+
+# LCD setup (usually address 0x27 or 0x3f, bus 1)
+lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
+              cols=16, rows=2, charmap='A00', auto_linebreaks=True)
+
+# Clear and display message
+lcd.clear()
+lcd.write_string('Hello World!')
+
+time.sleep(5)
+lcd.clear()
