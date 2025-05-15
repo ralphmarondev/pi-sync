b9919bc449ecf23a2760e434d78ddcd68695249d
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-05-15 21:33:05 +0800

    removed docs

--- docs/src/views/home/HomeIndex.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container mt-3">
-    <h3 class="text-primary">Hello there, Ralph Maron Eda is here!</h3>
-    <h5 class="text-secondary">Documentation of PiSync goes here!</h5>
-  </div>
-</template>
